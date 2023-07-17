8757861b889b7496a851a317401de303a58b1eb2
Krrish105 <jainkarishma76@gmail.com>
2023-07-17 11:38:29 +0530

    change

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,19 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
 	return (
 		<Html lang='en'>
 			<Head>
-				<script>{`
-					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-					})(window,document,'script','dataLayer','GTM-55MTT3C');`}</script>
+					})(window,document,'script','dataLayer','GTM-55MTT3C');`,
+					}}
+				/>
 				<meta name='description' content='Designo Multipage Website' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
