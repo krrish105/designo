6eba6aea22d1b4204906d6da32ada2951f23e903
Krrish105 <jainkarishma76@gmail.com>
2023-07-17 11:26:29 +0530

    change

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
 	return (
 		<Html lang='en'>
 			<Head>
-				<script type='text/javascript' async>{`
+				<script>{`
 					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
